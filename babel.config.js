794e1108148476106acc73809d840834b6494313
Hans Svensson <hans@rymdweb.com>
2025-11-24 01:51:20 +0100

    Added babel.config.js so things work with node24.

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['last 2 versions', 'not dead', '> 1%', 'not ie 11']
+        },
+        useBuiltIns: false,
+        modules: false
+      }
+    ]
+  ],
+  sourceType: 'module'
+};
