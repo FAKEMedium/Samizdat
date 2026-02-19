20231da41df5f09f7d02b8992adc3742eb1952b1
Hans Svensson <hans@rymdweb.com>
2026-02-19 16:42:12 +0100

    Manager panel fixes

--- templates/manager/index.js
+++ templates/manager/index.js
@@ -1,14 +1,17 @@
 const userServices = ['customer', 'invoice', 'domain', 'zone', 'database', 'email'];
+const u = window.username ?? '';
+const sa = window.superadmin ?? 0;
+const adm = window.admin ?? 0;
 
-if (username) {
+if (u) {
   document.getElementById('noservices').classList.add('d-none');
   document.querySelectorAll('.cardcol').forEach(card => {
     const service = card.id.replace('cardcol-', '');
-    if (superadmin || admin || userServices.includes(service)) {
+    if (sa || adm || userServices.includes(service)) {
       card.classList.remove('d-none');
     }
   });
-  if (superadmin || admin) {
+  if (sa || adm) {
     document.querySelectorAll('.cardcol .admin').forEach(el => el.classList.remove('admin'));
   }
 }
