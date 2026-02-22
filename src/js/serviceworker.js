2e027107b4c5b9b03be49e3a7b347b8e4239733e
Hans Svensson <hans@rymdweb.com>
2026-02-22 18:50:21 +0100

    Unregister old serviceworker url.

--- src/js/serviceworker.js
+++ src/js/serviceworker.js
@@ -1,6 +1,15 @@
 // Service Worker registration (using URL from body data attribute)
 const swUrl = document.body?.dataset?.swUrl;
 if ('serviceWorker' in navigator && swUrl) {
+    // Unregister any stale service workers at wrong URLs
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        for (const reg of registrations) {
+            if (reg.active && reg.active.scriptURL !== new URL(swUrl, location.href).href) {
+                reg.unregister();
+            }
+        }
+    });
+
     navigator.serviceWorker
         .register(swUrl, { scope: '/' })
         .then(function (reg) {
