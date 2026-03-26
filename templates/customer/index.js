c641e1d2d3778b60c01a0c363180ea95c1bc1d63
Hans Svensson <hans@rymdweb.com>
2026-03-26 22:00:05 +0100

    Show which customers an account is tied  to.

--- templates/customer/index.js
+++ templates/customer/index.js
@@ -32,20 +32,44 @@ function getCustomers(){
 }
 
 function populate(formdata) {
-  let customers = formdata.customers;
-  let searchterm = formdata.searchterm;
-  let snippet = ''
-  customers = customers.sortBy('-customerid');
-  for (const customer of customers) {
-    snippet += `
-      <tr data-customerid="${customer.customerid}">
-        <td><a class="w-auto" href="<%== url_for('customer_index') %>/${customer.customerid}">${customer.customerid}</a></td>
-        <td>${customer.company.replace(searchterm, '<b>' + searchterm + '</b>')}</td>
-        <td>${customer.firstname.replace(searchterm, '<b>' + searchterm + '</b>')}</td>
-        <td>${customer.lastname.replace(searchterm, '<b>' + searchterm + '</b>')}</td>
-      </tr>`;
+  const customers = (formdata.customers || []);
+  const isAdmin = formdata.admin !== 0;
+
+  if (isAdmin) {
+    const searchterm = formdata.searchterm || '';
+    let snippet = '';
+    customers.sortBy('-customerid');
+    for (const customer of customers) {
+      snippet += `
+        <tr data-customerid="${customer.customerid}">
+          <td><a class="w-auto" href="<%== url_for('customer_index') %>/${customer.customerid}">${customer.customerid}</a></td>
+          <td>${(customer.company || '').replace(searchterm, '<b>' + searchterm + '</b>')}</td>
+          <td>${(customer.firstname || '').replace(searchterm, '<b>' + searchterm + '</b>')}</td>
+          <td>${(customer.lastname || '').replace(searchterm, '<b>' + searchterm + '</b>')}</td>
+        </tr>`;
+    }
+    document.querySelector('#customers tbody').innerHTML = snippet;
+    document.querySelector('#customers').classList.remove('d-none');
+  } else {
+    if (customers.length === 0) {
+      document.querySelector('#no-affiliations').classList.remove('d-none');
+    } else {
+      let snippet = '';
+      customers.sortBy('customerid');
+      for (const customer of customers) {
+        const name = [customer.firstname, customer.lastname].filter(Boolean).join(' ');
+        snippet += `
+          <tr data-customerid="${customer.customerid}">
+            <td><a class="w-auto" href="<%== url_for('customer_index') %>/${customer.customerid}">${customer.customerid}</a></td>
+            <td>${customer.company || ''}</td>
+            <td>${name}</td>
+            <td>${customer.roles || ''}</td>
+          </tr>`;
+      }
+      document.querySelector('#mycustomers tbody').innerHTML = snippet;
+      document.querySelector('#mycustomers').classList.remove('d-none');
+    }
   }
-  document.querySelector('#customers tbody').innerHTML = snippet + `\n          `;
 }
 
 getCustomers();
